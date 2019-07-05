a56909c073ad7c12d0e2b8cf2ce3927407351006
linarsy <lina.stihi@gmail.com>
2019-07-05 17:44:56 +0300

    Вносит правки по замечаниям наставника v3

--- src/index.js
+++ src/index.js
@@ -1,34 +1,36 @@
 import readlineSync from 'readline-sync';
 
-const levelCount = 3;
+const lastLevel = 3;
 
-const flow = (getPuzzle, descriptionGame) => {
-  console.log(`Welcome to the Brain Games!\n${descriptionGame}\n`);
+const flow = (getLevelTask, descriptionGame) => {
+  console.log('Welcome to the Brain Games!');
+  console.log(`${descriptionGame}\n`);
   const playerName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${playerName}!\n`);
+  let levelsCount = 0;
 
-  const playGame = (level = 0) => {
-    const [question, answer] = getPuzzle();
-    const currentLevel = level + 1;
-    console.log(`Question: ${question}`);
-    const actual = readlineSync.question('Your answer: ');
-
-    if (actual !== `${answer}`) {
-      console.log(`'${actual}' is wrong answer ;(. Correct answer was '${answer}'.\nLet's try again, ${playerName}!`);
-      return;
-    }
-
-    console.log('Correct!');
-
-    if (currentLevel === levelCount) {
+  const inter = () => {
+    if (levelsCount === lastLevel) {
       console.log(`Congratulations, ${playerName}!`);
       return;
     }
 
-    playGame(currentLevel);
+    const [question, answer] = getLevelTask();
+    console.log(`Question: ${question}`);
+    const actual = readlineSync.question('Your answer: ');
+
+    if (actual !== `${answer}`) {
+      console.log(`'${actual}' is wrong answer ;(. Correct answer was '${answer}'.`);
+      console.log(`Let's try again, ${playerName}!`);
+      return;
+    }
+
+    console.log('Correct!');
+    levelsCount += 1;
+    inter();
   };
 
-  playGame();
+  inter();
 };
 
 export default flow;
